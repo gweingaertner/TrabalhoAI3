522eb6b7a14c262a532d3953a2c7c2028a3685e3
Guilherme <guilherme.weingaertner@gmail.com>
2022-06-15 16:04:40 -0300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,13 @@
+roleta = []
+
+lista_items = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+count = 1
+for i in lista_items:
+    for j in range(count):
+        roleta.append(i)
+    count += 1
+
+print(len(roleta))
+print(roleta)
+
+print("HELLO WORLD")
