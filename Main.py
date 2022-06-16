5ed7324f34f7e8b8f33b14dae6551b615ed4d31f
Guilherme <guilherme.weingaertner@gmail.com>
2022-06-16 18:14:24 -0300

    add cidade.mat

--- Main.py
+++ Main.py
@@ -1,3 +1,7 @@
+import random as rd
+import numpy as np
+import math as mat
+
 roleta = []
 
 lista_items = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
@@ -6,8 +10,6 @@ for i in lista_items:
     for j in range(count):
         roleta.append(i)
     count += 1
+data = np.loadtxt('cidades.mat')
+print(data[0])
 
-print(len(roleta))
-print(roleta)
-
-print("HELLO WORLD")
